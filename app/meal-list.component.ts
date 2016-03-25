58b2d33165acc6fb60b97ad195ced5a3ae40fdda
Oskar Radon <oskarradonkimball@gmail.com>
2016-03-25 09:05:15 -0700

    Add import for pipe to meal list component

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -4,12 +4,13 @@ import { MealComponent } from './meal.component';
 import { Meal } from './meal.model';
 import { EditMealDetailsComponent } from './edit-meal-details.component';
 import { NewMealComponent } from './new-meal.component';
+import { CaloriePipe } from './calorie.pipe';
 
 @Component({
   selector: 'meal-list',
   inputs: ['mealList'],
   outputs: ['onMealSelect'],
-  pipes: [MealPipe],
+  pipes: [CaloriePipe],
   directives: [MealComponent, EditMealDetailsComponent, NewMealComponent],
   template: `
     <select (change)="onChange($event.target.value)" class="filter">
@@ -17,7 +18,7 @@ import { NewMealComponent } from './new-meal.component';
       <option value="unhealthy">Foods with more than 300 calories</option>
       <option value="healthy">Foods with less than 300 calories</option>
     </select>
-    <meal-display *ngFor="#currentMeal of mealList | pints:filterDone"
+    <meal-display *ngFor="#currentMeal of mealList | calories:filterDone"
     (click)="mealClicked(currentMeal)"
     [meal]="currentMeal">
     </meal-display>
