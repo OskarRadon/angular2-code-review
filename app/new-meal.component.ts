405e71a7c36a52232493e6559d53d75a7e69bacd
Oskar <oskarradonkimball@gmail.com>
2016-03-25 10:55:04 -0700

    Remove extra closing div tag

--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -5,13 +5,12 @@ import { Meal } from './meal.model';
   selector: 'new-meal',
   outputs: ['onSubmitNewMeal'],
   template: `
-  <div class="meal-form">
+  <div class="new-meal-form">
     <h4>Create Meal: </h4>
     <input placeholder="Name" class="input-sm" #newName>
-    <input placeholder="Brand" class="input-sm" #newDetails>
-    <input placeholder="Alcohol Content" class="input-sm" #newCalories>
-    <button (click)="addMeal(newName, newDetails, newCalories)" class="create btn-success btn-sm add-button">Add</button>
-    </div>
+    <input placeholder="Details" class="input-sm" #newDetails>
+    <input placeholder="Calories" class="input-sm" #newCalories>
+    <button (click)="addMeal(newName, newDetails, newCalories)" class="create btn-success btn-sm add-button">Add Meal</button>
   </div>
   `
 })
@@ -23,7 +22,7 @@ export class NewMealComponent {
   addMeal(mealName: HTMLInputElement, mealDetails: HTMLInputElement, mealCalories: HTMLInputElement) {
     var newMeal = new Meal(mealName.value, mealDetails.value, parseInt(mealCalories.value));
     this.onSubmitNewMeal.emit(newMeal);
-    console.log(newMeal);
+    // console.log(newMeal);
     mealName.value = "";
     mealDetails.value = "";
     mealCalories.value = "";
