7af83a073a9a871802158309d7a59c9baf9094b0
Oskar Radon <oskarradonkimball@gmail.com>
2016-03-25 08:20:48 -0700

    Create root component and main.ts

---
+++ app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { MealListComponent } from './meal-list.component';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'my-app',
+  directives: [MealListComponent],
+  template: `
+    <div class= "container">
+      <h1>Meals:</h1>
+      <meal-list
+      [mealList]="meals"
+      (onMealSelect)="mealWasSelected($event)">
+      </meal-list>
+    <div>
+  `
+})
+export class AppComponent {
+  public kegs: Meal[];
+  constructor() {
+    this.meals = [
+      new Meal("Popcorn", "Went to see a movie", 350),
+      new Meal("Burrito", "I love burritos!!", 700),
+      new Meal("Tiramisu", "Very fancy", 550)
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log(clickedMeal);
+  }
+}
