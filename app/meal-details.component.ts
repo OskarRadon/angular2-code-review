cc7e24753ec1e00a06d7b55de8eb5703bff3d0de
Oskar <oskarradonkimball@gmail.com>
2016-03-25 11:51:07 -0700

    Change edit meal component to view details component

--- app/meal-details.component.ts
+++ app/meal-details.component.ts
@@ -2,19 +2,23 @@ import { Component } from 'angular2/core';
 import { Meal } from './meal.model';
 
 @Component({
-  selector: 'edit-meal-details',
+  selector: 'meal-details',
   inputs: ['meal'],
   template: `
     <div class="meal-form">
-      <h4>Name: </h4>
+      <h2>{{ meal.name }}, {{ meal.calories }} cal</h2>
+      <h3><i>{{ meal.details }}</i></h3>
+
+      <h3>Edit details</h3>
+      <h5>Name: </h5>
       <input [(ngModel)]="meal.name" class="input-sm meal-name-form"/>
-      <h4>Details: </h4>
+      <h5>Details: </h5>
       <input [(ngModel)]="meal.details" class="input-sm meal-name-form"/>
-      <h4>Calories: </h4>
+      <h5>Calories: </h5>
       <input [(ngModel)]="meal.calories" class="input-sm meal-name-form"/>
     </div>
   `
 })
-export class EditMealDetailsComponent {
+export class MealDetailsComponent {
   public meal: Meal;
 }
