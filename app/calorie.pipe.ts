125e0f66f38de12d8f3a80daf02acf7ad90ce9ba
Oskar Radon <oskarradonkimball@gmail.com>
2016-03-25 09:04:49 -0700

    Create calorie filter pipe

---
+++ app/calorie.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Pipe({
+  name: "calories",
+  pure: false
+})
+export class CaloriePipe implements PipeTransform {
+  transform(input: Meal[], args) {
+    var Healthiness = args[0];
+    if (Healthiness === "unhealthy") {
+      return input.filter((keg) => {
+        return (keg.calories > 300);
+      });
+    } if (Healthiness === "healthy") {
+      return input.filter((keg) => {
+        return (keg.calories < 300);
+      });
+    } else {
+      return input;
+    }
+  }
+}
