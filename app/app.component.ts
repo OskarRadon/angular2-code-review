f27d2e8752a28911fac1777698cb86df362abc3f
Oskar <oskarradonkimball@gmail.com>
2016-03-25 10:47:42 -0700

    Add dummy data back in

--- app/app.component.ts
+++ app/app.component.ts
@@ -19,12 +19,12 @@ export class AppComponent {
   public meals: Meal[];
   constructor() {
     this.meals = [
-      new Meal("Popcorn", "Went to see a movie", 350)
-      // new Meal("Burrito", "I love burritos!!", 700),
-      // new Meal("Tiramisu", "Very fancy", 550)
+      new Meal("Popcorn", "Went to see a movie", 350),
+      new Meal("Burrito", "I love burritos!!", 700),
+      new Meal("Tiramisu", "Very fancy", 550)
     ];
   }
   mealWasSelected(clickedMeal: Meal): void {
-    // console.log(clickedMeal); // This is where the view details will go
+    console.log(clickedMeal);
   }
 }
