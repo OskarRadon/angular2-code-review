071c424bf70b73d1236dc1d645d04e61a427fa95
Oskar Radon <oskarradonkimball@gmail.com>
2016-03-25 08:43:33 -0700

    Add edit meal component

---
+++ app/edit-meal-details.component.ts
@@ -0,0 +1,20 @@
+import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'edit-meal-details',
+  inputs: ['meal'],
+  template: `
+    <div class="meal-form">
+    <h4>Name: </h4>
+    <input [(ngModel)]="meal.name" class="input-sm meal-name-form"/>
+    <h4>Details: </h4>
+    <input [(ngModel)]="meal.brand" class="input-sm meal-name-form"/>
+    <h4>Calories: </h4>
+    <input [(ngModel)]="meal.alcoholContent" class="input-sm meal-name-form"/>
+    </div>
+  `
+})
+export class EditMealDetailsComponent {
+  public meal: Meal;
+}
