a572bac039ec3c691ced4755fd826e69eec5b3e3
Oskar Radon <oskarradonkimball@gmail.com>
2016-03-25 08:43:17 -0700

    Add meal list component

---
+++ app/meal-list.component.ts
@@ -0,0 +1,50 @@
+
+import { Component, EventEmitter } from 'angular2/core';
+import { MealComponent } from './meal.component';
+import { Meal } from './meal.model';
+import { EditMealDetailsComponent } from './edit-meal-details.component';
+import { NewMealComponent } from './new-meal.component';
+
+@Component({
+  selector: 'meal-list',
+  inputs: ['mealList'],
+  outputs: ['onMealSelect'],
+  pipes: [MealPipe],
+  directives: [MealComponent, EditMealDetailsComponent, NewMealComponent],
+  template: `
+    <select (change)="onChange($event.target.value)" class="filter">
+      <option value="all">All Inventory</option>
+      <option value="unhealthy">Foods with more than 300 calories</option>
+      <option value="healthy">Foods with less than 300 calories</option>
+    </select>
+    <meal-display *ngFor="#currentMeal of mealList | pints:filterDone"
+    (click)="mealClicked(currentMeal)"
+    [class.fancy]="currentMeal.price > 5"
+    [class.cheap]="currentMeal.price <= 5"
+    [meal]="currentMeal">
+    </meal-display>
+    <edit-meal-details *ngIf="selectedMeal" [meal]="selectedMeal">
+    </edit-meal-details>
+    <new-meal (onSubmitNewMeal)="createMeal($event)"></new-meal>
+  `
+})
+export class MealListComponent {
+  public mealList: Meal[];
+  public onMealSelect: EventEmitter < Meal > ;
+  public selectedMeal: Meal;
+  public filterDone: string = "all";
+  constructor() {
+    this.onMealSelect = new EventEmitter();
+  }
+  mealClicked(clickedMeal: Meal): void {
+    this.selectedMeal = clickedMeal;
+    this.onMealSelect.emit(clickedMeal);
+    console.log(this.selectedMeal);
+  }
+  createMeal(createdMeal: Meal): void {
+    this.mealList.push(createdMeal)
+  }
+  onChange(filterOption) {
+    this.filterDone = filterOption;
+  }
+}
